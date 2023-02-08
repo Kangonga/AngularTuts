7ba40059972a7e9cea87d0eab7dff953368e7e72
Kangonga <guchu413@gmail.com>
2023-02-08 17:19:50 +0300

    property, eveent and two way binding

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent {
+export class BooksComponent{
+  name="clean code";
+  author="robert martin"
+  isDisabled = false
+  propertyBound = "Learning property binding"
+  eventValue:string = ""
+  handleClick(){
+    this.isDisabled = !this.isDisabled
+  }
+  handleInput(event:any){
+    this.eventValue = event.target.value
+  }
+
 
 }
